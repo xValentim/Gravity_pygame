1dd07676bdc6f85528b89f14c90be05bb96c3963
Gabriel Valentim <gabriel.valentim.blis@hotmail.com>
2021-04-05 11:58:06 -0300

    Cálculos físicos

--- fisica.py
+++ fisica.py
@@ -8,6 +8,6 @@ def force(G, m1, m2, d):
 def distancia(x1, y1, x2, y2):
     return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
 
-#Calcula aceleracao
+#Calcula aceleracão
 def calcula_aceleracao(force, m):
     return force / m
